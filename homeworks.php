8485d7f631615ba9a52904c2296d6de441dc5971
MiloslavStekrt <stekrt18@gmail.com>
2020-12-08 23:47:13 +0100

    going to work homeworks

--- homeworks.php
+++ homeworks.php
@@ -6,19 +6,31 @@
   }
   $title = "BMS - Homeworks";
   include_once 'clear/header.php';
+
+  include_once 'include/db-connect.php';
+  $stmt = $conn->prepare("SELECT * FROM `classes` WHERE `teacher`=?")->execute([$_SESSION['id']]);
+  if($data == true){
+    $data = $stmt->fetchAll();
+  }else {
+    $data = ['name'=>'none'];
+  }
+  $stmt = null;
+  $conn = null;
+
 ?>
 <main>
   <section class="sidebar">
+    <select class="" name="">
+      <?php foreach ($data as $dat): ?>
+        <a href="homeworks.php?class=<?php echo $dat['name'] ?>"></a>
+      <?php endforeach; ?>
+    </select>
     <h1>Homeworks</h1>
+    <p><?php echo $_SESSION['id'] ?></p>
     <p><a href="">Ut - 192/12 c ucebnice</a></p>
     <p><a href="">Ut - 192/12 c ucebnice</a></p>
     <p><a href="">Ut - 192/12 c ucebnice</a></p>
-    <p><a href="">Ut - 192/12 c ucebnice</a></p>
-    <p><a href="">Ut - 192/12 c ucebnice</a></p>
-    <p><a href="">Ut - 192/12 c ucebnice</a></p>
-    <p><a href="">Ut - 192/12 c ucebnice</a></p>
-    <p><a href="">Ut - 192/12 c ucebnice</a></p>
-    <p><a href="">Ut - 192/12 c ucebnice</a></p>
+    <p><a href="new_homework.php">new Homework</a></p>
   </section>
   <section class="control">
     <article class="avg">
