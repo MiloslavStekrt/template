449d1915490ecb9ccdb585246c93538a6f5378e8
MiloslavStekrt <stekrt18@gmail.com>
2020-12-08 22:24:23 +0100

    add working option to view all students in school and add they to classes

--- include/class_user.php
+++ include/class_user.php
@@ -1,15 +1,16 @@
 <?php
+$id_class = $_POST["id_class"];
+$list_user = $_POST["list_user"];
 
-if(!isset($_POST['button'])){
+if(!isset($_POST['button']) || empty($list_user)){
   header("location: /admin/lessons.php");
   die();
 }
-$id_class = $_POST["id_class"];
-$list_user[] = $_POST["list_user[]"];
+
 include_once 'db-connect.php';
 foreach($list_user as $user){
   echo "insert items .... ".$id_class." and ".$user;
   $conn->prepare("INSERT INTO `class_student`(`id_class`, `id_user`) VALUES (?, ?)")->execute([$id_class, $user]);
 }
-// header("location: /admin/lessons.php?id=".$id_class);
-// die();
+header("location: /admin/lessons.php?id=".$id_class);
+die();
--- include/db-connect.php
+++ include/db-connect.php
@@ -1,14 +1,27 @@
 <?php
 
-function generate($dbname, $nick, $pwd){
-  try{
-    $conn = new PDO("mysql:host=localhost;dbname=$dbname", $nick, $pwd);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  } catch(PDOException $e) {
-    header("location: /");
-    die();
-  }
-  return $conn;
-}
+$servername = "localhost";
+$username = "root";
+$password = "AdminLuser";
 
-$conn = generate("beam-ms","root", "AdminLuser");
+// function generate($dbname, $nick, $pwd){
+//   try{
+//     $conn = new PDO("mysql:host=localhost;dbname=$dbname", $nick, $pwd);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   } catch(PDOException $e) {
+//     header("location: /");
+//     die();
+//   }
+//   return $conn;
+// }
+//
+//
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=beam-ms", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
