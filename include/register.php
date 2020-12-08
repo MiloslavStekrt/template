af99512dfdac807e1d4ae0e47643abba2747d782
MiloslavStekrt <stekrt18@gmail.com>
2020-12-08 23:46:44 +0100

    After registration user must login.

--- include/register.php
+++ include/register.php
@@ -14,16 +14,5 @@ $email = $_POST['email'];
 include_once 'newuser.php';
 register($name, $pwd, $pwdr, $role, $email, "register.php", $conn);
 
-if($role == "teacher"){
-  $role = 2;
-}elseif($role == "student"){
-  $role = 1;
-}
-
-$_SESSION['name'] = $name;
-$_SESSION['email'] = $email;
-$_SESSION['role'] = $role;
-$_SESSION['auth'] = false;
-
-header("location: /school.php");
+header("location: /login.php");
 die();
