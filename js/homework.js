99a87d8d9e999f864864a3a7ce5130a9544c665b
MiloslavStekrt <stekrt18@gmail.com>
2020-12-12 12:01:16 +0100

    homeworkjs with vuejs script for homeworks

---
+++ js/homework.js
@@ -0,0 +1,21 @@
+// const Counter = new Vue({
+//   el: '#app',
+//   data:{
+//     return{
+//       couter: 0
+//     }
+//   },
+//   method: {
+//     mouted(){
+//       setInterval(()=>{
+//         this.couter++
+//       }, 1000)
+//     }
+//   }
+// });
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     message:  new Date().toLocaleString()
+//   }
+// })
