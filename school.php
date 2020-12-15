0946ba8d1a6f4aced3a3f41b0becb6192f78d103
MiloslavStekrt <stekrt18@gmail.com>
2020-12-15 16:36:43 +0100

    add more dinamic schedule to html

--- school.php
+++ school.php
@@ -12,8 +12,51 @@ include_once 'clear/header.php';?>
     </span>
   </section>
   <section class="schedule">
-    <h2>You have today</h2>
+    <h2>Schedule</h2>
+    <article class="hodiny">
+      <p> </p>
+      <p>1</p>
+      <p>2</p>
+      <p>3</p>
+      <p>4</p>
+      <p>5</p>
+      <p>6</p>
+      <p>7</p>
+      <p>8</p>
+    </article>
+    <article class="monday">
+      <p>Monday</p>
+      <p>Thruesday</p>
+      <p>Frieday</p>
+    </article>
+    <article class="tuesday">
+      <p>Tuesday</p>
+
+    </article>
+    <article class="wensday">
+      <p>Wensday</p>
+
+    </article>
     <article class="lessons">
+      <h3>Monday</h3>
+      <p>M</p>
+      <p>M</p>
+      <p>AJ</p>
+      <p>CJ</p>
+      <p>L</p>
+      <p> </p>
+    </article>
+    <article class="lessons">
+      <h3>Tuesday</h3>
+      <p>M</p>
+      <p>M</p>
+      <p>AJ</p>
+      <p>CJ</p>
+      <p>L</p>
+      <p> </p>
+    </article>
+    <article class="lessons">
+      <h3>Wenday</h3>
       <p>M</p>
       <p>M</p>
       <p>AJ</p>
